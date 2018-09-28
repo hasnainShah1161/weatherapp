0ef7839295bc9fc5e82f48f01f1c6a9793df7f87
hasnain shah <hasnainshah1161@gmail.com>
2018-09-28 23:28:39 +0500

    first commit
    Co-Authored-By: hasnain shah <hasnainshah1161@gmail.com>

--- weather.js
+++ weather.js
@@ -1,21 +1,11 @@
 class Weather {
-
-// constructor(city,state){
-//     const city = document.getElementById('citySearch').value;
-//     const state = document.getElementById('stateSearch').value;
-// }
-
-    async getWeather(city,state){
-
-        
-
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&APPID=f466d21f45c81f59b422c912019a33c2
+  async getWeather(city, state) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&APPID=f466d21f45c81f59b422c912019a33c2
 
         `);
 
-        const weatherResponse = await response.json();
+    const weatherResponse = await response.json();
 
-       
-        return weatherResponse;
-}
+    return weatherResponse;
+  }
 }
